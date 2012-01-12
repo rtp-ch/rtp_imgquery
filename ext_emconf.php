cbccf75f33e59e80039b9ca939c5d4eac53aaf76
Marcel Greter <mgr@rtp.ch>
2012-01-12 17:13:30 +0100

    [~TASK] Improved client side js

--- ext_emconf.php
+++ ext_emconf.php
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '1.0.2',
+	'version' => '1.0.3',
 	'constraints' => array(
 		'depends' => array(
 		),
