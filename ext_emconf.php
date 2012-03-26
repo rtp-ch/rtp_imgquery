f44793f520e7997e87e0297220c9fd9ca2962b5d
Manuel Stofer <mst@rtp.ch>
2012-03-26 16:37:37 +0200

    [~CONFIGURATION] Bumped version no. (0.6.6)

--- ext_emconf.php
+++ ext_emconf.php
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.6.5',
+	'version' => '0.6.6',
 	'constraints' => array(
 		'depends' => array(
 		),
