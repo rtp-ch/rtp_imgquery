15a8a11b82f68774967c8010178ea4d3ab1169c2
Mario Rimann <mario@rimann.org>
2013-03-24 20:26:10 +0100

    [FEATURE] Compatibility with TYPO3 6.0+
    The change adds a new namespaced class that contains basically
    the same stuff than the previous class. All occurrences of t3lib_div
    have been replaced with the new namespaced class.
    
    This change should not break compatibility with TYPO3 versions
    below 6.0 as the old Xclass stuff is kept as is.
    
    So far it looks as if it's working on 6.1-dev, but needs some
    deeper testing!

--- ext_localconf.php
+++ ext_localconf.php
@@ -12,3 +12,9 @@
     // TODO: Check versions for availability of "tslib_content_Image" --> if(version_compare(TYPO3_version, '4.3.0', '>'))
     $GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]['XCLASS']['tslib/content/class.tslib_content_image.php']
                 = t3lib_extMgm::extPath('rtp_imgquery') . 'Classes/Xclass/class.ux_tslib_content_image.php';
+
+
+	// new XCLASS mapping that is used by TYPO3 6.0+
+	$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['TYPO3\\CMS\\Frontend\\ContentObject\\ImageContentObject'] = array(
+		'className' => 'Rtp\\RtpImgquery\\Xclass\\ImageContentObject'
+	);
