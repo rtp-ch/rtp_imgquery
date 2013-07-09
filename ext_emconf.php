77f888e570550680ea87a9c5eee55923c4fd1760
Simon Tuck <stu@rtp.ch>
2013-07-09 12:15:27 +0200

    [RELEASE] 0.7.2

--- ext_emconf.php
+++ ext_emconf.php
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.7.1',
+	'version' => '0.7.2',
 	'constraints' => array(
 		'depends' => array(
 		),
